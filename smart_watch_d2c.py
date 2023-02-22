39828d87aef53b2ee7885bfab0a387c8736d6332
Talha794 <76178966+Talha794@users.noreply.github.com>
2023-02-22 17:45:07 +0500

    smart waych d2c

--- smart_watch_d2c.py
+++ smart_watch_d2c.py
@@ -16,7 +16,6 @@ from constants import *
 
 requestsToSend = []
 
-
 class Server(asyncore.dispatcher):
     def __init__(self, address):
         asyncore.dispatcher.__init__(self)
@@ -59,13 +58,10 @@ class ClientHandler(asyncore.dispatcher):
 
     def handle_read(self):
         rawTCPData = self.recv(1024*3)
-        print('Raw Received Data: () ""', len(rawTCPData), rawTCPData.rstrip())
-        self.data_parsing(rawTCPData)
-        self.calling_provisioner_to_send_data()
-        self.parsedData = {}
-       # rawHexData = binascii.hexlify(rawTCPData).decode()
-       # print("Raw Hex Data: ", rawHexData , ", Len: ", len(rawHexData))
-       
+        # print('Raw Received Data: () ""', len(rawTCPData), rawTCPData.rstrip())
+        rawHexData = binascii.hexlify(rawTCPData).decode()
+        print("Raw Hex Data: ", rawHexData , ", Len: ", len(rawHexData))
+        self.data_parsing(rawHexData)
 
     def handle_close(self):
         print('Closing the TCP Socket, handling close!')
@@ -81,188 +77,19 @@ class ClientHandler(asyncore.dispatcher):
             else:
                 print("Packet Sending Failure for: , Response: ", self.id, response.status_code)
             await client.aclose()
-
     def calling_provisioner_to_send_data(self):
         dataToSend = json.dumps(self.parsedData)
-        print(dataToSend)
-        url = "http://" + PROVISIONER_SERVER + ":" + PROVISIONER_PORT + "/data/straight2IoTHub?&data=" + dataToSend
+        url = "http://" + PROVISIONER_SERVER + ":" + PROVISIONER_PORT + "/data/iol?&data=" + dataToSend
         try:
             asyncio.run(self.sendAsyncReq(url))
         except:
             print("Data Sending Exception occurred: ", self.id)
         self.parsedData = {}
 
+    def data_parsing(self, rawHexData):
+        # rawHexData raw data parsing
+        pass
 
-    def data_parsing(self, rawTCPData):
-        raw = rawTCPData.decode('utf-8')
-        print('Raw Received: ', raw, "length:", len(raw))
-        splittedData = raw.split(',')
-        print(splittedData)
-        comType = splittedData[0].split('*')
-        print(comType[3])
-        print(comType[1])
-        if comType[3] == "WAD":
-            locationRequestResponse = "[SG*8800000015*000C*RAD,GPS,EH]"
-            #locationRequestResponse = "01"
-            hex = bytes(locationRequestResponse,'utf-8')
-            hex2 = binascii.hexlify(hex)
-            self.data_to_write.insert(0, binascii.unhexlify(hex2))
-            speed = splittedData[9]
-            direction = splittedData[10]
-            altitude = splittedData[11]
-            battery = splittedData[14]
-            walkStep = splittedData[16]
-            lat = splittedData[5]
-            latDir = splittedData[6] 
-            lon = splittedData[7] 
-            lonDir = splittedData[8] 
-            #print(lat,",",latDir,",",lon,",",lonDir)
-            self.id = comType[1]
-            self.parsedData['id'] = self.id
-            self.parsedData['lat'] = lat
-            self.parsedData['lon'] = lon
-            self.parsedData['spd'] = speed
-            self.parsedData['alt'] = altitude
-            self.parsedData['dir'] = direction
-            self.parsedData['b'] = battery
-            self.parsedData['sc'] = walkStep
-        
-            #self.parsedData['latDir'] = latDir
-            #self.parsedData['lonDir'] = lonDir
-            #print(self.parsedData)
-            #json_object = json.dumps(self.parsedData, indent = 4)
-            #print(json_object)
-        elif comType[3] == "LK":
-            if comType[2] == "0002":
-                locationRequestResponse = "[SG*8800000015*0002*LK]"
-                #locationRequestResponse = "01"
-                hex = bytes(locationRequestResponse,'utf-8')
-                hex2 = binascii.hexlify(hex)
-                self.data_to_write.insert(0, binascii.unhexlify(hex2))
-            elif comType[2] == "000D":
-                locationRequestResponse = "[SG*8800000015*0002*LK]"
-                #locationRequestResponse = "01"
-                hex = bytes(locationRequestResponse,'utf-8')
-                hex2 = binascii.hexlify(hex)
-                self.data_to_write.insert(0, binascii.unhexlify(hex2))
-                steps = splittedData[1] 
-                turnOver = splittedData[2] 
-                battery = splittedData[3] 
-                self.id = comType[1]
-                self.parsedData['id'] = self.id
-                self.parsedData['steps'] = steps            
-                self.parsedData['to'] = turnOver            
-                self.parsedData['b'] = battery
-        elif comType[3] == "UD":
-            locationRequestResponse = "NO"
-            #locationRequestResponse = "01"
-            hex = bytes(locationRequestResponse,'utf-8')
-            hex2 = binascii.hexlify(hex)
-            self.data_to_write.insert(0, binascii.unhexlify(hex2))
-            speed = splittedData[8]
-            direction = splittedData[9]
-            altitude = splittedData[10]
-            battery = splittedData[13]
-            walkStep = splittedData[15]
-            lat = splittedData[4]
-            latDir = splittedData[5] 
-            lon = splittedData[6] 
-            lonDir = splittedData[7] 
-            #print(lat,",",latDir,",",lon,",",lonDir)
-            self.id = comType[1]
-            self.parsedData['id'] = self.id
-            self.parsedData['lat'] = lat
-        elif comType[3] == "UD2":
-            locationRequestResponse = "NO"
-            #locationRequestResponse = "01"
-            hex = bytes(locationRequestResponse,'utf-8')
-            hex2 = binascii.hexlify(hex)
-            self.data_to_write.insert(0, binascii.unhexlify(hex2))
-            speed = splittedData[8]
-            direction = splittedData[9]
-            altitude = splittedData[10]
-            battery = splittedData[13]
-            walkStep = splittedData[15]
-            lat = splittedData[4]
-            latDir = splittedData[5] 
-            lon = splittedData[6] 
-            lonDir = splittedData[7] 
-            #print(lat,",",latDir,",",lon,",",lonDir)
-            self.id = comType[1]
-            self.parsedData['id'] = self.id
-            self.parsedData['lat'] = lat
-            self.parsedData['lon'] = lon
-            self.parsedData['spd'] = speed
-            self.parsedData['alt'] = altitude
-            self.parsedData['dir'] = direction
-            self.parsedData['b'] = battery
-            self.parsedData['sc'] = walkStep 
-            self.parsedData['spd'] = speed
-            self.parsedData['alt'] = altitude
-            self.parsedData['dir'] = direction
-            self.parsedData['b'] = battery
-            self.parsedData['sc'] = walkStep
-        elif comType[3] == "AL":
-            locationRequestResponse = "[SG*8800000015*0002*AL]"
-            #locationRequestResponse = "01"
-            hex = bytes(locationRequestResponse,'utf-8')
-            hex2 = binascii.hexlify(hex)
-            self.data_to_write.insert(0, binascii.unhexlify(hex2))
-            speed = splittedData[8]
-            direction = splittedData[9]
-            altitude = splittedData[10]
-            battery = splittedData[13]
-            walkStep = splittedData[15]
-            lat = splittedData[4]
-            latDir = splittedData[5] 
-            lon = splittedData[6] 
-            lonDir = splittedData[7] 
-            #print(lat,",",latDir,",",lon,",",lonDir)
-            self.id = comType[1]
-            self.parsedData['id'] = self.id
-            self.parsedData['lat'] = lat
-            self.parsedData['lon'] = lon
-            self.parsedData['spd'] = speed
-            self.parsedData['alt'] = altitude
-            self.parsedData['dir'] = direction
-            self.parsedData['b'] = battery
-            self.parsedData['sc'] = walkStep 
-            self.parsedData['spd'] = speed
-            self.parsedData['alt'] = altitude
-            self.parsedData['dir'] = direction
-            self.parsedData['b'] = battery
-            self.parsedData['sc'] = walkStep
-        elif comType[3] == "WG":
-            speed = splittedData[8]
-            direction = splittedData[9]
-            altitude = splittedData[10]
-            battery = splittedData[13]
-            walkStep = splittedData[15]
-            lat = splittedData[4]
-            latDir = splittedData[5] 
-            lon = splittedData[6] 
-            lonDir = splittedData[7] 
-            #print(lat,",",latDir,",",lon,",",lonDir)
-            self.id = comType[1]
-            self.parsedData['id'] = self.id
-            self.parsedData['lat'] = lat
-            self.parsedData['lon'] = lon
-            self.parsedData['spd'] = speed
-            self.parsedData['alt'] = altitude
-            self.parsedData['dir'] = direction
-            self.parsedData['b'] = battery
-            self.parsedData['sc'] = walkStep 
-            self.parsedData['spd'] = speed
-            self.parsedData['alt'] = altitude
-            self.parsedData['dir'] = direction
-            self.parsedData['b'] = battery
-            self.parsedData['sc'] = walkStep
-            locationRequestResponse = "SG*8800000015*0021*RG,BASE,"+lat+","+latDir+","+lon+","+lonDir
-            #locationRequestResponse = "01"
-            hex = bytes(locationRequestResponse,'utf-8')
-            hex2 = binascii.hexlify(hex)
-            self.data_to_write.insert(0, binascii.unhexlify(hex2))         
-                
 def dataHandler():
     while True:
         global requestsToSend
